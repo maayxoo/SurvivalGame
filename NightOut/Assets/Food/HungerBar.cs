e316b9c9c8b1199135c9c88af0de26462c9b7643
mxrtinvu <60140779+mxrtinvu@users.noreply.github.com>
2022-01-11 21:09:05 +0000

    Update food

---
+++ NightOut/Assets/Food/HungerBar.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+
+public class HungerBar : MonoBehaviour
+{
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private PlayerInput playerInput;
+
+
+    public Slider HungerSlider;
+
+    public static float Hunger;
+    float maxHunger = 100f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        Hunger = maxHunger;
+        moveAction = playerInput.actions["Movement"];
+        jumpAction = playerInput.actions["Jump"];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        HungerSlider.value = Hunger;
+
+        if (Hunger > 0)
+        {
+
+          Hunger -= 0.2f * Time.deltaTime;
+
+          if(input != Vector2.zero)
+          {
+            Hunger -= 2f * Time.deltaTime;
+          }
+          if (jumpAction.triggered)
+          {
+                Hunger -= 4f * Time.deltaTime;
+            }
+        }
+      }
+    }
